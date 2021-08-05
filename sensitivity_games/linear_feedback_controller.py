2cd3609e044813273b2ad445f6d2da6d92bac9b8
Jonathan Salfity <jonathan.salfity@gmail.com>
2021-08-05 12:11:33 -0700

    updating K calculation format

--- sensitivity_games/linear_feedback_controller.py
+++ sensitivity_games/linear_feedback_controller.py
@@ -9,32 +9,34 @@ class LinearFeedbackController(RegulatorController):
     def __init__(self,
                  dynamics,
                  xf):
-
         super().__init__(dynamics, xf)
-        # self.K = torch.randn(dynamics.input_dim,
-        #                      dynamics.state_dim,requires_grad=True)
 
         self.K = torch.tensor([[1.0, 0, 0, 0],
                               [0, 1.0, 0, 0]],
                               requires_grad=True)
 
     def solve_dare_for_k(self, Q, R):
-        # http://www.mwm.im/lqr-controllers-with-python/
-        # first, try to solve the ricatti equation
+        '''
+        Calculation taken from http://www.mwm.im/lqr-controllers-with-python/
+        '''
         X = torch.tensor(linalg.solve_discrete_are(self.dynamics.A,
                                                    self.dynamics.B.detach(),
                                                    Q,
                                                    R), dtype=torch.float32)
 
         # compute the LQR gain
-        inv = linalg.inv((self.dynamics.B.T@X@self.dynamics.B).detach()+R)
+        term1 = torch.tensor(
+            linalg.inv((self.dynamics.B.T @ X @ self.dynamics.B).detach() + R),
+            dtype=torch.float32)
 
-        term1 = torch.tensor(inv, dtype=torch.float32)
-        term2 = self.dynamics.B.T.detach()@X@self.dynamics.A
-        K = term1@term2
+        term2 = self.dynamics.B.T.detach() @ X @ self.dynamics.A
+
+        K = term1 @ term2
         return K
 
     def get_eigenVals_eigenVecs(self):
+        '''
+        '''
         eigVals, eigVecs = linalg.eig(self.dynamics.A -
                                       self.dynamics.B.detach()@self.K.detach())
         return eigVals, eigVecs
