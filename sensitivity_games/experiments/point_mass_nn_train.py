e64dd09c9f74a1f2b6799573ac5382720da1ed3f
jsalfity <j.salfity@utexas.edu>
2021-08-30 14:45:45 -0500

    feature nn controller branch

---
+++ sensitivity_games/experiments/point_mass_nn_train.py
@@ -0,0 +1,126 @@
+import argparse
+import numpy as np
+import random
+
+from sensitivity_games.linear_feedback_controller import (
+    LinearFeedbackController)
+from sensitivity_games.trajectory import Trajectory
+from sensitivity_games.point_mass import PointMass
+from sensitivity_games.quadratic import Quadratic
+from sensitivity_games.trajectory_cost import TrajectoryCost
+
+import torch
+
+
+def _setup_parser():
+    """Set up Python's ArgumentParser with params"""
+    parser = argparse.ArgumentParser(add_help=False)
+
+    parser.add_argument("--viz", type=bool, default=False)
+    parser.add_argument("--verbose", type=bool, default=True)
+    parser.add_argument("--verbose_freq", type=int, default=100)
+    parser.add_argument("--epochs", type=int, default=5000)
+    parser.add_argument("--lr", type=float, default=1e-3)
+    parser.add_argument("--T", type=int, default=10)
+    parser.add_argument("--x0_limit", nargs='+',
+                        type=float, default=[5, 0, 5, 0])
+    parser.add_argument("--x0_rand", type=bool, default=True)
+    parser.add_argument("--xf", nargs='+', type=float, default=[0, 0, 0, 0])
+    parser.add_argument("--block", type=bool, default=False)
+
+    return parser
+
+
+def run_experiment():
+
+    parser = _setup_parser()
+    args = parser.parse_args()
+
+    # initial conditions
+    xf = torch.tensor(np.array(args.xf))
+
+    # create dynamics
+    dynamics = PointMass(m=1,
+                         dt=0.1,
+                         theta={'dm': torch.zeros(1, requires_grad=True)})
+
+    # create controller
+    controller = LinearFeedbackController(dynamics, xf)
+
+    traj_cost = TrajectoryCost()
+    traj_cost.addStateCost(Quadratic(n=xf[0], d=0))  # x position
+    # traj_cost.addStateCost(Quadratic(n=xf[1], d=1))  # x velocity
+    traj_cost.addStateCost(Quadratic(n=xf[2], d=2))  # y position
+    # traj_cost.addStateCost(Quadratic(n=xf[3], d=3))  # y velocity
+
+    traj_cost.addControlCost(Quadratic(n=0, d=0))  # x
+    traj_cost.addControlCost(Quadratic(n=0, d=1))  # y
+
+    traj_cost.addThetaCost('dm', Quadratic(n=0, d=0, weight=25))
+
+    # optimizer_k = torch.optim.Adam([controller.K], lr=args.lr)
+
+    optimizer_theta = torch.optim.Adam(dynamics.theta.values(),
+                                       lr=args.lr)
+    optimizer_theta.param_groups[0]['lr'] *= -1  # hack for gradient ascent
+
+    for n in range(args.epochs):
+
+        # perturb with thetas
+        dynamics.perturb()
+
+        x0 = torch.tensor([float(args.x0_limit[0]),
+                           float(args.x0_limit[1]),
+                           float(args.x0_limit[2]),
+                           float(args.x0_limit[3])])
+
+        if args.x0_rand is True:
+            x0 = torch.tensor([random.uniform(-args.x0_limit[0],
+                                              args.x0_limit[0]),
+                               random.uniform(-args.x0_limit[1],
+                                              args.x0_limit[1]),
+                               random.uniform(-args.x0_limit[2],
+                                              args.x0_limit[2]),
+                               random.uniform(-args.x0_limit[3],
+                                              args.x0_limit[3])])
+
+        # unroll trajectory
+        traj = Trajectory(dynamics, xf, args.T)
+        X, U = traj.unroll(x0, controller)
+
+        # do gradient update
+        total_cost = traj_cost.evaluate(X, U, dynamics)
+
+        optimizer_k.zero_grad()
+        optimizer_theta.zero_grad()
+
+        # dtotal/dtheta[dm], dtotal/dstate_cost dtotal/dcontrol_cost
+        total_cost.backward()
+
+        optimizer_k.step()
+        optimizer_theta.step()
+
+        # print info
+        if (n % args.verbose_freq == 0 and args.verbose) or n == args.epochs-1:
+            eigVals, _ = controller.get_eigenVals_eigenVecs()
+            print("EPOCH: {}".format(n))
+            print("total_cost: {}".format(total_cost))
+            print("controller.K: {}".format(controller.K))
+            print("controller.K.grad: {}".format(controller.K.grad))
+            print("dynamics.theta['dm']: {}".format(dynamics.theta['dm']))
+            print("dynamics.theta['dm'].grad: {}".format(
+                                                    dynamics.theta['dm'].grad))
+            print("eigVals: {}".format(eigVals))
+            print("x0: {}".format(x0))
+            print("xf (goal): {}".format(xf))
+            print("xf (actual): {}".format(X[-1]))
+            print("________________________________________________")
+
+            if args.viz:
+                traj.visualize(args.block)
+
+    return dynamics, controller, traj, traj_cost
+
+
+if __name__ == "__main__":
+    run_experiment()
--- sensitivity_games/nn_controller.py
+++ sensitivity_games/nn_controller.py
@@ -14,3 +14,24 @@ class NeuralNetworkController(Controller):
         '''
         '''
         return self.model.forward(x)
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(4, 4),
+            nn.ReLU(),
+            nn.Linear(4, 4),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
